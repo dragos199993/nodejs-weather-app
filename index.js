c48a52eed2075e604e5f89cde1dbee09060ad13a
Nedsilon <dragos199993@gmail.com>
2018-06-24 01:19:56 +0300

    updated to promise syntax

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const yargs = require("yargs");
 
 const location = require("./google-geocode");
 const weather = require("./weather-api");
-const util = require('./utilities');
+const util = require("./utilities");
 
 let argv = yargs
   .options({
@@ -16,11 +16,18 @@ let argv = yargs
   .help()
   .alias("help", "h").argv;
 
-location.geocode(argv.a, (error, data) => {
-  if (error) console.log(error);
-    console.log(`You are in ${data.address} and...`);
-    weather.getWeather(data, (error, data) => {
-        if(error) console.log(error);
-            console.log(`Outside it is ${data.currentState} with a temperature of ${data.currentTemp}C felted as ${data.feltTemp}C.`);
-    });
-});
+location
+  .geocode(argv.a)
+  .then(data => {
+    weather
+      .getWeather(data)
+      .then(res =>
+        console.log(
+          `Outside it is ${res.currentState} with a temperature of ${
+            res.currentTemp
+          }C felted as ${res.feltTemp}C.`
+        )
+      )
+      .catch(err => console.log(err));
+  })
+  .catch(err => console.log(err));
--- weather-api.js
+++ weather-api.js
@@ -1,30 +1,28 @@
-
 const request = require("request");
-const api = require('./api');
+const api = require("./api");
 
 apiKey = api.apiKey;
-const getWeather = (data, callback) => {
-  request(
-    {
-      url: `https://api.darksky.net/forecast/${apiKey}/${data.latitude},${
-        data.longitude
-      }`,
-      json: true
-    },
-    (error, response, body) => {
-      if (!error && response.statusCode === 200) {
-        callback(undefined, {
+const getWeather = data => {
+  return new Promise((resolve, reject) => {
+    request(
+      {
+        url: `https://api.darksky.net/forecast/${apiKey}/${data.latitude},${
+          data.longitude
+        }`,
+        json: true
+      },
+      (error, response, body) => {
+        if (!error && response.statusCode === 200)
+          resolve({
             currentTemp: parseInt((body.currently.temperature - 32) / 1.8),
             feltTemp: parseInt((body.currently.apparentTemperature - 32) / 1.8),
             currentState: body.currently.summary
-        })
-      } else {
-        console.log("Something went wrong in weather api");
+          });
+        reject("Something went wrong with Weather API");
       }
-    }
-  );
+    );
+  });
 };
-
 module.exports = {
-    getWeather
+  getWeather
 };
